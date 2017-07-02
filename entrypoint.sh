23a4c735e6497a6738ddb5d13527f763d5e7adde
blacktop <blacktop@users.noreply.github.com>
2017-07-01 22:35:29 -0600

    FIX!

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-hugo server -w --bind="0.0.0.0" -v -p 80 -d /docs --theme="$MALICE_DOCS_THEME"
+# default to `hugo-material-docs` if theme is not supplied
+hugoTheme="${MALICE_DOCS_THEME:-hugo-material-docs}"
+
+hugo server -w --bind="0.0.0.0" -v -p 80 -d /docs --theme="$hugoTheme"
