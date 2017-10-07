1f0896af32b79119ef942e4e3a6d08d4d6594000
blacktop <blacktop@users.noreply.github.com>
2017-10-07 00:50:27 -0600

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 # default to `hugo-material-docs` if theme is not supplied
 hugoTheme="${MALICE_DOCS_THEME:-hugo-material-docs}"
 
-hugo server -D --watch=false --bind="0.0.0.0" -v -p 80 --theme="$hugoTheme"
+hugo server -vDEF --watch=false --bind="0.0.0.0" --port=80 --theme="$hugoTheme"
